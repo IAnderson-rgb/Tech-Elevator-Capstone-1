7b94fe89bf681747fa0c956a25ae7abe3742c290
Will Ortquist <will.ortquist@gmail.com>
2020-02-04 15:02:57 -0500

    Submission of Week four day two pair activity

--- 18_FileIO_Writing_out/pair-exercise/dotnet/FindAndReplace/Program.cs
+++ 18_FileIO_Writing_out/pair-exercise/dotnet/FindAndReplace/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FindAndReplace
 {
@@ -6,7 +7,48 @@ namespace FindAndReplace
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter the phrase you would like to replace");
+            string targetPhrase = Console.ReadLine();
+
+            Console.WriteLine("Please enter the phrase you would like it to be replaced with");
+            string replacePhrase = Console.ReadLine();
+
+
+            Console.WriteLine("What is the fully qualified name of the file that should be searched?");
+            string pathToBeRead = Console.ReadLine();
+
+            Console.WriteLine("Please enter the path destination for your new file?");
+            string newFilePath = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(pathToBeRead))
+                {
+                    using (StreamWriter sw = new StreamWriter(newFilePath, true))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+
+                           // string changedLine = "";
+                            string line = sr.ReadLine();
+                            if (line.Contains(targetPhrase))
+                            {
+                                line = line.Replace(targetPhrase, replacePhrase);
+
+                            }
+                            sw.WriteLine(line);
+
+
+                        }
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+               Console.WriteLine("Something went wrong");
+                Console.WriteLine(e.Message);
+
+            }
         }
     }
 }
