b5ecd919fb89255b2964dff9aa1d37495f8d12ea
Will Ortquist <will.ortquist@gmail.com>
2020-03-23 12:15:08 -0400

    Refined VendingMachine application

--- 19_Capstone/dotnet/Capstone/Program.cs
+++ 19_Capstone/dotnet/Capstone/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using System.IO;
+using Capstone.Classes;
 
 namespace Capstone
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // When program is launched this stocks the Vending machine and creates a new vending machine.
+            // TODO: fix hard coded path
+            string inventoryFile = @"C:\Users\Student\workspace\week-4-pair-exercises-c-team-1\19_Capstone\dotnet\Example Files/VendingMachine.txt";
+            VendingMachine machine = new VendingMachine();
+            using (StreamReader sr = new StreamReader(inventoryFile))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    machine.GetInventory(line);
+                }
+            }
+
+            //Now that we have created our vending machine, and stocked it, we are going to display our menu to the user
+            Menu startMenu = new CLIMenu(machine);
+            startMenu.Run();
         }
     }
 }
